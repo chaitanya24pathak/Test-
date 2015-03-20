ca5c225df8ef55af7886bd8843b530fd7b2b9594
chaitanya24pathak <chaitanya.pathak@bigtree.in>
2015-03-20 14:43:21 +0530

    Usher_Point
    Usher_Point

---
+++ js/JavaScript.js
@@ -0,0 +1,42 @@
+﻿function callAjaxEx(strURL, strCallBack) {
+    var strReturn = "";
+
+    try {
+        var XMLHttpRequestObject;
+        if (window.XMLHttpRequest) {
+            XMLHttpRequestObject = new XMLHttpRequest();
+        }
+        else if (window.ActiveXObject) {
+            XMLHttpRequestObject = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        if (XMLHttpRequestObject) {
+            XMLHttpRequestObject.open("GET", strURL, true);
+            XMLHttpRequestObject.onreadystatechange = function () {
+                if (XMLHttpRequestObject.readyState == 4 && XMLHttpRequestObject.status == 200) {
+                    try {
+                        strReturn = XMLHttpRequestObject.responseText;
+                        if (strCallBack.toString().length > 0)
+                            eval(strCallBack + "(\"" + strReturn + "\");");
+                    }
+                    catch (e) {
+                        alert(e);
+                    }
+                    return;
+                    // }
+                }
+            }
+            XMLHttpRequestObject.send(null);
+        }
+    }
+    catch (e) {
+        alert(e.message + ";" + e.toString());
+    }
+    return strReturn;
+}
+
+function subReturn(ret) {
+    var StrRet = ret;
+    alert(ret);
+
+
+}
